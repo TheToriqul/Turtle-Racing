e319f9dd9087d13f8c416e17717b0efa7fc78a70
Md Toriqul Islam <137867238+TheToriqul@users.noreply.github.com>
2024-05-23 21:32:21 +0800

    code updated

---
+++ app.py
@@ -0,0 +1,11 @@
+def get_number_of_racers():
+    racers = 0
+    while True:
+        racers = input("Enter the number of players (2 - 10): ")
+        if racers.isdigit():
+            racers = int(racers)
+        else:
+            print("Please enter a number between 2 and 10.")
+            continue
+
+        print("Enter the number of players")
